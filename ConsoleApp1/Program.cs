65b85ded9ada7a57e711df0eaf21e160948f7d06
Leti? Boris <RA207-2021@ftn.ns>
2023-10-24 09:16:41 +0200

    ide

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+
+
+bool hasThree = false;
+
+while (n > 0)
+{
+    int t = n % 10;
+    if (t == 3)
+    {
+        Console.WriteLine("Number contains 3");
+        break;
+    }
+    n = n / 10;
+}
+
+if(!hasThree)
+{
+    Console.WriteLine("Number NOT contains 3");
+
+}
